fd9c3a9336760af8b1244157ddfb31158d300afd
Swen Mun <longfinfunnel@gmail.com>
2019-12-06 13:33:10 +0900

    Handle cancellation properly

--- Libplanet/Net/Swarm.cs
+++ Libplanet/Net/Swarm.cs
@@ -2239,6 +2239,11 @@ namespace Libplanet.Net
                 {
                     await ProcessRequest(req, cancellationToken);
                 }
+                catch (OperationCanceledException)
+                {
+                    _logger.Information("Cancellation requsted; shutdown runtime...");
+                    throw;
+                }
                 catch (Exception e)
                 {
                     if (req.Retryable)
